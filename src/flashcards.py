b87f68a54412fed7e4c21ee3b90fa030b0ce2e4e
Blake Edwards <blakeedwards823@gmail.com>
2025-03-01 20:19:52 -0500

    updates to frontend, added focus mode

--- src/flashcards.py
+++ src/flashcards.py
@@ -52,4 +52,4 @@ def save():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
